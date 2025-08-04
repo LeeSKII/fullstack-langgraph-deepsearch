54f6cf300e6223682784b286790fd6e625af23df
leeski <540470452@qq.com>
2025-08-04 15:00:05 +0800

    update style

--- frontend/src/pages/Chat.jsx
+++ frontend/src/pages/Chat.jsx
@@ -183,48 +183,58 @@ function Chat() {
   };
 
   return (
-    <div>
-      <div className="flex-1 w-full h-full overflow-y-auto bg-white rounded-lg shadow p-6">
-        <div className="flex flex-col gap-3">
-          {messages.map((message, i) => {
-            if (message.role === "assistant") {
-              return (
-                <Bubble
-                  key={i}
-                  placement="start"
-                  content={message.content}
-                  avatar={{ icon: <Bot />, style: { background: "#1d3acdff" } }}
-                  loading={message.status === "loading"}
-                />
-              );
-            } else {
-              return (
-                <Bubble
-                  key={i}
-                  placement="end"
-                  content={message.content}
-                  avatar={{ icon: <User />, style: { background: "#87d068" } }}
-                />
-              );
-            }
-          })}
+    <div className="container mx-auto p-2 h-screen flex flex-col bg-gray-100">
+      <div className="flex flex-col gap-4 h-11/12">
+        <div className="flex-1 w-full h-full overflow-y-auto bg-white rounded-lg shadow p-6">
+          <div className="flex flex-col gap-3">
+            {messages.map((message, i) => {
+              if (message.role === "assistant") {
+                return (
+                  <Bubble
+                    key={i}
+                    placement="start"
+                    content={message.content}
+                    avatar={{
+                      icon: <Bot />,
+                      style: { background: "#1d3acdff" },
+                    }}
+                    loading={message.status === "loading"}
+                  />
+                );
+              } else {
+                return (
+                  <Bubble
+                    key={i}
+                    placement="end"
+                    content={message.content}
+                    avatar={{
+                      icon: <User />,
+                      style: { background: "#87d068" },
+                    }}
+                  />
+                );
+              }
+            })}
+          </div>
         </div>
       </div>
-      <Sender
-        submitType="shiftEnter"
-        value={sendValue}
-        onChange={(v) => {
-          setSendValue(v);
-        }}
-        placeholder="Press Shift + Enter to send message"
-        loading={isStreaming}
-        onSubmit={() => {
-          startStream(endpoint);
-        }}
-        onCancel={() => {
-          stopStream();
-        }}
-      />
+      <div className="flex flex-row gap-2 justify-center items-center w-full mt-2 h-1/12 bg-white rounded-lg shadow p-4 z-10">
+        <Sender
+          submitType="shiftEnter"
+          value={sendValue}
+          onChange={(v) => {
+            setSendValue(v);
+          }}
+          placeholder="Press Shift + Enter to send message"
+          loading={isStreaming}
+          onSubmit={() => {
+            startStream(endpoint);
+          }}
+          onCancel={() => {
+            stopStream();
+          }}
+        />
+      </div>
     </div>
   );
 }
